d357e60d8f239943995d0adfe4290141b7732e7a
Stu-Gotz <gamer691@gmail.com>
2023-03-20 13:41:50 +0000

    added login page

--- login.php
+++ login.php
@@ -1,7 +1,49 @@
-<?php include 'header.php'; ?>
-<h1 style="font-family: sans-serif; color: darkgray;">You have logged in as John Doe.</h1>
-<form action="autologin.php" method="POST">
-    <input type="submit" value="Continue" name="login">
+<?php include 'header.php';
+require_login();
+if (isset($_POST['submit'])) {
+    $inputs = [];
+    $errors = [];
+
+    [$inputs, $errors] = filter($_POST, [
+        'username' => 'string | required',
+        'password' => 'string | required'
+    ]);
+
+    if ($errors) {
+        redirect_with('login.php', ['errors' => $errors, 'inputs' => $inputs]);
+    }
+
+    // if login fails
+    if (!login($inputs['username'], $inputs['password'])) {
+
+        $errors['login'] = 'Invalid username or password';
+
+        redirect_with('login.php', [
+            'errors' => $errors,
+            'inputs' => $inputs
+        ]);
+    }
+}
+?>
+<!-- <h1 style="font-family: sans-serif; color: darkgray;">You have logged in as John Doe.</h1> -->
+<!-- login form -->
+<form action="<?php echo $_SERVER['PHP_SELF']; ?>" method="POST">
+    <h1>Login</h1>
+    <div>
+        <label for="username">Username:</label>
+        <input type="text" name="username" id="username" value="<?= $inputs['username'] ?? '' ?>">
+        <small><?= $errors['username'] ?? '' ?></small>
+    </div>
+    <div>
+        <label for="password">Password:</label>
+        <input type="password" name="password" id="password">
+        <small><?= $errors['password'] ?? '' ?></small>
+    </div>
+    <div>
+        <input name="submit" value="Log In" type="submit" />
+        <!-- client can click register if their account is not registered to login -->
+        <a href="register.php">Register</a>
+    </div>
 </form>
 
 <p>Interested in investing with us? <a href="register.php">Register here!</a></p>
